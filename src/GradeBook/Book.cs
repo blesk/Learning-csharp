a924f85ef87e24d18037a5bda628d808de2115bd
Michal Marciniszyn <michal.marciniszyn@solarwinds.com>
2020-03-04 17:56:05 +0100

    Initial Commit

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace GradeBook
+{
+    public class Book
+    {
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public double GetLowestGrade()
+        {
+            var result = double.MaxValue;
+            foreach(double i in grades)
+            {
+                result = Math.Min(i, result);
+            }
+            return result;
+        }
+
+        public double GetHighestGrade()
+        {
+            var result = double.MinValue;
+            foreach (double i in grades)
+            {
+                result = Math.Max(i, result);
+            }
+            return result;
+        }
+
+        public double GetAverageGrade()
+        {
+            var avg = 0.0;
+
+            foreach(double i in grades)
+            {
+                avg += i;
+            }
+            return avg / grades.Count;
+        }
+
+        public Statistics GetStatistics()
+        {
+            var result = new Statistics();
+
+
+            result.Average = GetAverageGrade();
+            result.Low = GetLowestGrade();
+            result.High = GetHighestGrade();
+
+            return result;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        List<double> grades;
+        private string name;
+    }
+}
---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Confederacy of Dunces");
+            book.AddGrade(98.2);
+            book.AddGrade(18.3);
+            book.AddGrade(99.9);
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"Statistics for: {book.GetName()}");
+            Console.WriteLine($"Average:{stats.Average:N3}");
+            Console.WriteLine($"High:   {stats.High}");
+            Console.WriteLine($"Low:    {stats.Low}");
+        }
+    }
+}
